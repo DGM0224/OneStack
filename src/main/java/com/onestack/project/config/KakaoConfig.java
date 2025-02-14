d4673745e56285bd9df4bd5033ef9b1b17a058f1
Kyle <rlaxoals9977@gmail.com>
2025-02-14 19:47:56 +0900

    소셜로그인 제거 2

--- src/main/java/com/onestack/project/config/KakaoConfig.java
+++ src/main/java/com/onestack/project/config/KakaoConfig.java
@@ -8,20 +8,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class KakaoConfig {
     
-    @Value("${kakao.client.id}")
-    private String clientId;
-    
-    @Value("${kakao.redirect.uri}")
-    private String redirectUri;
-    
-    @Bean(name = "kakao.auth.url")
-    public String kakaoAuthUrl() {
-        return String.format("https://kauth.kakao.com/oauth/authorize"
-                + "?response_type=code"
-                + "&client_id=%s"
-                + "&redirect_uri=%s"
-                + "&scope=profile_nickname,profile_image"
-                + "&prompt=login consent",
-                clientId, redirectUri);
-    }
+//    @Value("${kakao.client.id}")
+//    private String clientId;
+//
+//    @Value("${kakao.redirect.uri}")
+//    private String redirectUri;
+//
+//    @Bean(name = "kakao.auth.url")
+//    public String kakaoAuthUrl() {
+//        return String.format("https://kauth.kakao.com/oauth/authorize"
+//                + "?response_type=code"
+//                + "&client_id=%s"
+//                + "&redirect_uri=%s"
+//                + "&scope=profile_nickname,profile_image"
+//                + "&prompt=login consent",
+//                clientId, redirectUri);
+//    }
 } 
